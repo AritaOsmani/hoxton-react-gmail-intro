601b3e186c7d5918652aca3cb31e2a2f10fcf5d8
Arita Osmani <aritaaosmani@gmail.com>
2022-01-04 16:20:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+
+import Header from './components/Header'
+import LeftMenu from './components/LeftMenu'
+import EmailView from './components/EmailView'
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <LeftMenu />
+
+      <EmailView />
+    </div>
+  )
+}
+
+export default App
